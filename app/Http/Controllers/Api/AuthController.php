30647a0045d4b341edd0a865bde39b713f2d4d4d
unknown <sobasic4me@gmail.com>
2024-09-05 17:17:29 +0800

    laravel react js

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    //
+    public function signup(Signuprequest $request) 
+    {
+        $data = $request->validated();
+        /** @var \App\Models\User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        //return response(['user' => $user, 'token' => $token]); // simplify this using compact
+        return response(compact('user', 'token' ));
+
+    }
+
+    public function login(LoginRequest $request) 
+    {
+        $credentials = $request->validated();
+
+        // if the inputs are incorrect then return error message
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Provided email or password is incorrect'
+            ], 422);
+        }
+
+        
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user', 'token'));
+        
+
+    }
+
+    public function logout(Request $request) 
+    {
+        
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        
+        return response('', 204); // 204 means that the request succesfully work but nothing to return
+        
+
+    }
+}
